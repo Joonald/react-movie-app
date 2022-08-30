3a145cf5b5b658e8e8dc0b5963c6214c0b79c5f3
Josh Solomon <jonnypn9@gmail.com>
2022-08-30 14:46:21 -0700

    added movie posters from fetched api

--- src/components/MoviePoster.js
+++ src/components/MoviePoster.js
@@ -1,6 +1,40 @@
 // Single Movie Poster 
+import { useState, useEffect } from 'react';
+import { endPointNowPlaying, endPointPopular, endPointTopRated, endPointUpcoming, apiKey, secureUrl, imgSize} from '../globals/globalVariables';
 
 function MoviePoster () {
+    const [movieData, setMovieData] = useState(false);
 
+    useEffect( () => {
+        const fetchMovie = async () => {
+            const res = await fetch(`${endPointNowPlaying}${apiKey}`);
+            let data = await res.json();
+            setMovieData(data);
+        }
+        console.log('hi');
+        fetchMovie();
+    }, []);
+    
+    return (
+        <div>
+        {movieData.results?.map((movie) => 
+            <div key={movie.id}>
+                <img src={`${secureUrl}${imgSize}${movie.poster_path}`} alt="" />
+                <h2>{movie.title}</h2>
+                <span>
+                {movie.genre_ids.map((id) =>
+                    <p>{id}</p>
+                )}
+                </span> 
+                <p>
+                {movie.release_date}
+                </p>
+                <p>
+                {movie.overview}
+                </p>
+            </div>
+        )}
+        </div>
+    )
 };
 export default MoviePoster;
