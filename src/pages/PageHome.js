efa192176649bc4c5d4a3af55ab47cf456281682
Josh Solomon <jonnypn9@gmail.com>
2022-08-31 15:27:03 -0700

    added sort by buttons based on popular etc etc

--- src/pages/PageHome.js
+++ src/pages/PageHome.js
@@ -2,14 +2,48 @@
 
 // Imports
 import MoviePoster from "../components/MoviePoster";
+import { endPointNowPlaying, endPointPopular, endPointTopRated, endPointUpcoming, apiKey, secureUrl, imgSize} from '../globals/globalVariables';
+import { useState } from 'react';
+import Button from "../components/Button";
+import { sortByButtons } from "../globals/sortByButtons";
+
 
 function PageHome () {
+    const [sortMovie, setSortMovie] = useState(endPointNowPlaying)
+
+    function handleClick (value) {
+        switch (value) {
+            case 'now playing':
+                setSortMovie(endPointNowPlaying);
+                break;
+            case 'upcoming':
+                setSortMovie(endPointUpcoming);
+                break;    
+            case 'popular':
+                setSortMovie(endPointPopular);
+                break;
+            case 'top rated':
+                setSortMovie(endPointTopRated);
+                break;
+            default:
+        }
+    }
     return (
         <main>
             {/* hero banner */}
 
             {/* movie poster */}
-             <MoviePoster />
+            {sortByButtons.map((btn) => 
+             <Button
+                key={btn.value}
+                value={btn.value}
+                className={btn.type}
+                text={btn.text}
+                handleClick={handleClick}
+             />
+            )}
+           
+            <MoviePoster sort={sortMovie}/>
         </main>
     );
 };
