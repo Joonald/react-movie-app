78aa47ac2afe2b533c50eedd33a300d074af6837
Josy Li <josyliyh@gmail.com>
2022-09-14 15:21:30 -0700

    trailer

--- src/components/MovieBanner.js
+++ src/components/MovieBanner.js
@@ -1,12 +1,15 @@
 // Movie Banner
-import { useState, useEffect } from 'react';
+import React, {useState,useEffect } from 'react';
 import { Link } from 'react-router-dom';
-import { apiKey, secureUrl, imgSize, endPointNowPlaying} from '../globals/globalVariables';
+import { apiKey, secureUrl, imgSize, endPointNowPlaying,endPointSingleMovie, engLang} from '../globals/globalVariables';
+import ReactDOM from 'react-dom';
+import ModalVideo from 'react-modal-video';
 
 import Carousel from 'react-bootstrap/Carousel';
 
 function MovieBanner () {
-
+  const [isOpen, setOpen] = useState(false)
+  const [Trailer, setTrailer] = useState(false);
     const [movieBanner, setMovieBanner] = useState(false);
     useEffect( () => {
         const fetchMovie = async () => {
@@ -16,28 +19,67 @@ function MovieBanner () {
         }
         fetchMovie();
     });
-    console.log(movieBanner);
 
-    // const bannerList = movieData.banner?.slice(0, 5);
-      // console.log(bannerList);
+ const theMovieBanner = movieBanner.results?.slice(0, 5);
+
+
+ //trailer
+ let movie = "";
+    useEffect(() => {
+      const fetchTrailer = async() => {
+          const trailerlist = await fetch(`${endPointSingleMovie}${movie.id}/videos?${apiKey}${engLang}`);
+          let Trailer = await trailerlist.json();
+          setTrailer(Trailer);
+       
+      }
+      fetchTrailer();
+    },[movie.id]);
+
+  const trailerLink = () => {
+    const trailer = Trailer.results?.find (vid => vid.name === 'Official Trailer')
+          
+    return (
+        <div>
+      
+        <React.Fragment>
+        <ModalVideo channel='youtube' autoplay isOpen={isOpen} videoId={trailer?.key} onClose={() => setOpen(false)} />
+
+        <button className="trailer-button" onClick={()=> setOpen(true)}>Watch Trailer</button>
+        </React.Fragment>
+    
+      </div>
+    )
+  }
+
+  
         return (
-          <Carousel fade >
-             {movieBanner.results?.map((movie) => 
-            
+          <>
+       
+          
+          <Carousel  >
+             {theMovieBanner?.map((movie) => 
+         
           <Carousel.Item>
+           
             <img
               className="d-block w-100 carousel-poster"
              src={`${secureUrl}w780${movie.backdrop_path}`} alt={movie.title} 
             />
-            <Carousel.Caption>
-              <h3>{movie.title}</h3>
-            </Carousel.Caption>
-          </Carousel.Item>
           
-
+            <Carousel.Caption>
+              <div>
+            {/* {trailerLink(movie.id)} */}
+            </div>
+              <h3>{movie.title}</h3>
+             
+            </Carousel.Caption>
+             
+          </Carousel.Item>
+        
              )}
           
         </Carousel>
+        </>
         );
       }
       
