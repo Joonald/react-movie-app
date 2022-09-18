f7f5fcd3263fef13ed4e8a3ee4eedc0c6cd0bc84
Josy Li <josyliyh@gmail.com>
2022-09-18 11:57:22 -0700

    almost

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,11 +2,11 @@
 import logo from '../image/cloud-movie-logo.svg';
 import Search from './Search'
 import Nav from '../components/Nav';
-
+import { NavLink } from "react-router-dom";
 
 const Header = ({ title }) => (
     <header id='header-nav'>
-     <img className='header-logo' src={logo} alt='logo'/>
+     <NavLink to='/'><img className='header-logo' src={logo} alt='logo'/></NavLink>
     <Search/>
     <Nav/>
     </header>
--- src/components/Search.js
+++ src/components/Search.js
@@ -7,7 +7,7 @@ import { Link } from 'react-router-dom';
 import { apiKey, secureUrl, imgSize,endPointNowPlaying} from '../globals/globalVariables';
 import { GENRES } from '../globals/genreList';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faHeart } from '@fortawesome/free-solid-svg-icons';
+import { faDisplay, faHeart } from '@fortawesome/free-solid-svg-icons';
 import { useSelector, useDispatch } from 'react-redux';
 import { addFav, delFav } from '../features/favs/favSlice';
 
@@ -15,8 +15,7 @@ const Search= () => {
   const [suggestion, setSuggestion] = useState ([]);
   const[searchTerm, setSearchTerm] = useState('');
 
-  let matches=[];
-  const [movieData, setMovieData] = useState([]);
+  const navigate = useNavigate();
 
 
   useEffect( () => {
@@ -27,7 +26,6 @@ const Search= () => {
           // const res = await fetch(`https://api.themoviedb.org/3/discover/movie?${apiKey}&sort_by=popularity.desc&include_adult=false&include_video=false&with_watch_monetization_types=flatrate`);
           const res = await fetch(`https://api.themoviedb.org/3/search/movie?${apiKey}&language=en-US&query=${searchTerm}&page=1&include_adult=false`);
           let data = await res.json();
-          setMovieData(data.results);
           setSuggestion(data.results);
       }
       fetchMovie();
@@ -40,58 +38,50 @@ const Search= () => {
 
 
 
-
   const handleChange= (searchTerm) => {
       setSearchTerm(searchTerm);
       console.log(searchTerm)
-     
-    // if (searchTerm.length > 0) {
-    //   // matches = movieData.filter(movie => movie.title = )
-    //   matches = movieData.filter(movie => {
-    //   const regex = new RegExp(`${searchTerm}`, "gi");
-      
-    //   return movie.title.match(regex)
-      
-    //   })
-    //   }
-    //   setSuggestion(matches)
-    //   console.log(matches)
-    //   setSearchTerm(searchTerm)
-     
+
     } 
 
     const resetInputField = () => {
       setSearchTerm("")
     }
+
+  
     const handleSubmit= (event) => {
       event.preventDefault();
       resetInputField();
-
+      navigate(`/search/${searchTerm}`);
     } 
     const onSuggestHandler = (searchTerm) => {
       setSearchTerm(searchTerm)
       setSuggestion([])
+      setSearchTerm([])
       }
+
+  
   
  
       return (
    
         <div>
-        <form onSubmit={handleSubmit}>
-              
+      <form onSubmit={handleSubmit}>     
           <label>
-         
-            <input type="text" value={searchTerm} onChange={e => handleChange(e.target.value)} onBlur={() => setSuggestion([])}/>
+            <input   className='searchbar'  placeholder="Search for a movie" type="text"  value={searchTerm}  onChange={e => handleChange(e.target.value)} />
           </label>
-          <button type="submit"  value="Submit"><Link to={`/search/${searchTerm}`}>search</Link></button>    
-
-        </form>
+      </form>
+{/*            onBlur={setSuggestion([])}
+          onBlur={e => blurHandler()} */}
+        
+        {/* <div className="autocomplete" >
         {suggestion && suggestion.map((onesuggestion, i) =>
-          <div key={i} className="suggestion" onClick={() => onSuggestHandler(onesuggestion.title)}>{onesuggestion.title}
-                          <img src={`${secureUrl}${imgSize}${onesuggestion.poster_path}`} alt={onesuggestion.title} />
-
-          </div>
+          <div onMouseDown={(e) => e.preventDefault()} key={i} className="suggestion" onClick={() => onSuggestHandler(onesuggestion.title)}><Link to={`/single/${onesuggestion.id}`}>{onesuggestion.title}</Link>
+                    </div>
 )}
+        </div>  */}
+     
+      
         </div>
       )
     
